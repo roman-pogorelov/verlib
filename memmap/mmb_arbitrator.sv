5b1466c775ce096e6af3d65412815bb84ab571dc
Roman A. Pogorelov <roman.a.pogorelov@yandex.ru>
2017-11-04 14:28:42 +0300

    Реализован арбитр доступа нескольких ведущих интерфейса MemoryMapped с пакетным доступом к одному ведомому

--- memmap/mmb_arbitrator.sv
+++ memmap/mmb_arbitrator.sv
@@ -1,6 +1,6 @@
 /*
     //------------------------------------------------------------------------------------
-    //      Арбитр доступа нескольких ведущих интерфейса MemoryMapped с пакетным 
+    //      Арбитр доступа нескольких ведущих интерфейса MemoryMapped с пакетным
     //      доступом к одному ведомому
     mmb_arbitrator
     #(
@@ -79,13 +79,18 @@ module mmb_arbitrator
     //      Объявление сигналов
     logic [MASTERS - 1 : 0]                         request;
     logic [MASTERS - 1 : 0]                         grant;
+    logic [MASTERS - 1 : 0]                         burst_reg;
+    logic [BWIDTH - 1 : 0]                          write_cnt;
     logic [MASTERS - 1 : 0]                         read_position;
     logic                                           read_enable;
     logic                                           read_valid;
+    logic [BWIDTH - 1 : 0]                          read_bcnt;
+    logic [BWIDTH - 1 : 0]                          read_cnt;
+    logic                                           read_complete;
     
     //------------------------------------------------------------------------------------
     //      Сигналы запроса доступа со стороны ведущих
-    assign request = s_wreq | s_rreq;
+    assign request = (s_wreq | s_rreq) & ~burst_reg;
     
     //------------------------------------------------------------------------------------
     //      Арбитр доступа нескольких абонентов к одному ресурсу
@@ -115,7 +120,7 @@ module mmb_arbitrator
     
     //------------------------------------------------------------------------------------
     //      Коммутация сигнала отсутствия готовности
-    assign s_busy = ~grant | {MASTERS{m_busy}} | (s_rreq & ~{MASTERS{read_enable}});
+    assign s_busy = ~grant | {MASTERS{m_busy}} | (s_rreq & ~{MASTERS{read_enable}}) | burst_reg;
     
     //------------------------------------------------------------------------------------
     //      Разветвление данных чтения от ведомого ко всем ведущим
@@ -151,6 +156,48 @@ module mmb_arbitrator
     end
     
     //------------------------------------------------------------------------------------
+    //      Коммутация шины размера пакета
+    always_comb begin
+        for (int i = 0; i < BWIDTH; i++) begin
+            logic [MASTERS - 1 : 0] bcnt_comm;
+            for (int j = 0; j < MASTERS; j++) begin
+                bcnt_comm[j] = s_bcnt[j][i] & grant[j];
+            end
+            m_bcnt[i] = |bcnt_comm;
+        end
+    end
+    
+    //------------------------------------------------------------------------------------
+    //      Регистры признаков прохождения пакетной транзакции записи для
+    //      каждого ведущего
+    generate
+        genvar i;
+        for (i = 0; i < MASTERS; i++) begin: burst_reg_generation
+            always @(posedge reset, posedge clk)
+                if (reset)
+                    burst_reg[i] <= '0;
+                else if (burst_reg[i])
+                    burst_reg[i] <= ~(m_wreq & ~m_busy & (write_cnt == (BWIDTH > 1 ? 2 : 0)));
+                else
+                    burst_reg[i] <=  (m_wreq & ~m_busy & (m_bcnt != 1)) & ~(s_wreq[i] & ~s_busy[i]);
+        end
+    endgenerate
+    
+    //------------------------------------------------------------------------------------
+    //      Счетчик одной пакетной транзакции записи одного ведущего
+    initial write_cnt = {{BWIDTH - 1{1'b0}}, 1'b1};
+    always @(posedge reset, posedge clk)
+        if (reset)
+            write_cnt <= {{BWIDTH - 1{1'b0}}, 1'b1};
+        else if (m_wreq & ~m_busy)
+            if (write_cnt == 1)
+                write_cnt <= m_bcnt;
+            else
+                write_cnt <= write_cnt - 1'b1;
+        else
+            write_cnt <= write_cnt;
+    
+    //------------------------------------------------------------------------------------
     //      Генерация буфера коммутаций чтения необходимой длины
     generate
         if (RDPENDS > 1) begin: rd_direction_fifo_generation
@@ -159,25 +206,25 @@ module mmb_arbitrator
             //      на ядре от Altera
             ds_alt_scfifo
             #(
-                .DWIDTH             (MASTERS),          // Разрядность потока
-                .DEPTH              (RDPENDS),          // Глубина FIFO
-                .RAMTYPE            (RAMTYPE)           // Тип блоков встроенной памяти ("MLAB", "M20K", ...)
+                .DWIDTH             (MASTERS + BWIDTH),             // Разрядность потока
+                .DEPTH              (RDPENDS),                      // Глубина FIFO
+                .RAMTYPE            (RAMTYPE)                       // Тип блоков встроенной памяти ("MLAB", "M20K", ...)
             )
             rd_direction_fifo
             (
                 // Сброс и тактирование
-                .reset              (reset),            // i
-                .clk                (clk),              // i
+                .reset              (reset),                        // i
+                .clk                (clk),                          // i
                 
                 // Входной потоковый интерфейс
-                .i_dat              (grant),            // i  [DWIDTH - 1 : 0]
-                .i_val              (m_rreq & ~m_busy), // i
-                .i_rdy              (read_enable),      // o
+                .i_dat              ({grant, m_bcnt}),              // i  [DWIDTH - 1 : 0]
+                .i_val              (m_rreq & ~m_busy),             // i
+                .i_rdy              (read_enable),                  // o
                 
                 // Выходной потоковый интерфейс
-                .o_dat              (read_position),    // o  [DWIDTH - 1 : 0]
-                .o_val              (read_valid),       // o
-                .o_rdy              (m_rval)            // i
+                .o_dat              ({read_position, read_bcnt}),   // o  [DWIDTH - 1 : 0]
+                .o_val              (read_valid),                   // o
+                .o_rdy              (m_rval & read_complete)        // i
             ); // rd_direction_fifo
         end
         else begin: rd_direction_onereg_generation
@@ -185,23 +232,23 @@ module mmb_arbitrator
             //      Буфер на одной регистровой ступени для потокового интерфейса DataStream
             ds_onereg_buffer
             #(
-                .WIDTH      (MASTERS)           // Разрядность потокового интерфейса
+                .WIDTH      (MASTERS + BWIDTH)              // Разрядность потокового интерфейса
             )
             rd_direction_onereg_buffer
             (
                 // Сброс и тактирование
-                .reset      (reset),            // i
-                .clk        (clk),              // i
+                .reset      (reset),                        // i
+                .clk        (clk),                          // i
                 
                 // Входной потоковый интерфейс
-                .i_dat      (grant),            // i [WIDTH - 1 : 0]
-                .i_val      (m_rreq & ~m_busy), // i
-                .i_rdy      (read_enable),      // o
+                .i_dat      ({grant, m_bcnt}),              // i [WIDTH - 1 : 0]
+                .i_val      (m_rreq & ~m_busy),             // i
+                .i_rdy      (read_enable),                  // o
                 
                 // Выходной потоковый интерфейс
-                .o_dat      (read_position),    // o [WIDTH - 1 : 0]
-                .o_val      (read_valid),       // o
-                .o_rdy      (m_rval)            // i
+                .o_dat      ({read_position, read_bcnt}),   // o [WIDTH - 1 : 0]
+                .o_val      (read_valid),                   // o
+                .o_rdy      (m_rval & read_complete)        // i
             ); // rd_direction_onereg_buffer
         end
     endgenerate
@@ -209,5 +256,23 @@ module mmb_arbitrator
     //------------------------------------------------------------------------------------
     //      Коммутация сигналов подтверждения считываемых данных
     assign s_rval = read_position & {MASTERS{read_valid}} & {MASTERS{m_rval}};
+    
+    //------------------------------------------------------------------------------------
+    //      Счетчик ответов на запросы чтения одного ведущего
+    initial read_cnt = {{BWIDTH - 1{1'b0}}, 1'b1};
+    always @(posedge reset, posedge clk)
+        if (reset)
+            read_cnt <= {{BWIDTH - 1{1'b0}}, 1'b1};
+        else if (read_valid & m_rval)
+            if (read_complete)
+                read_cnt <= {{BWIDTH - 1{1'b0}}, 1'b1};
+            else
+                read_cnt <= read_cnt + 1'b1;
+        else
+            read_cnt <= read_cnt;
+    
+    //------------------------------------------------------------------------------------
+    //      Признак окончания пакетной транзакции чтения для одного ведущего
+    assign read_complete = (read_cnt == read_bcnt);
     
 endmodule: mmb_arbitrator
